52e328ccdc06b24763a313cf79477de162ab4a92
RivaldoTjio <rivaldohadiwinata@gmail.com>
2020-11-10 10:04:40 +0700

    bla bla

--- app/src/main/java/com/rivaldo/profilfakultas/Fakultas.kt
+++ app/src/main/java/com/rivaldo/profilfakultas/Fakultas.kt
@@ -1,5 +1,3 @@
 package com.rivaldo.profilfakultas
 
-import android.net.Uri
-
 data class Fakultas(val name: String, val desc: String, val desc2: String, val image: Int, val link: String,val email: String, val linkprogdi : Array<String>)
